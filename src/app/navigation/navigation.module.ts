15da869c7e3cb48fa534b76eda5a5546504701b8
calimadevteam <calimadevs@gmail.com>
2023-02-22 15:58:04 +0100

    added log in, sign up, home navigation to web mode

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { NavigationComponent } from './navigation.component';
+import { RouterModule } from '@angular/router';
+
 
 
 @NgModule({
   imports: [
     CommonModule,
     IonicModule,
+    RouterModule,
   ],
   declarations: [NavigationComponent],
   exports: [
