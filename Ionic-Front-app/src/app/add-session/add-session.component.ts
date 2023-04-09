04290bf5344b9bd4744a667940799c26ab3f6d11
kakas <e.kakas@student.fontys.nl>
2023-04-09 10:18:54 +0200

    changing ionic action flow

--- Ionic-Front-app/src/app/add-session/add-session.component.ts
+++ Ionic-Front-app/src/app/add-session/add-session.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddSessionComponent implements OnInit {
   constructor() { }
+  
   ngOnInit() {}
 
   onRatingUpdated(rating: number) {
