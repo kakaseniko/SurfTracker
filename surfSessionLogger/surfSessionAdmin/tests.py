998d6c9704a5473dad9491093f88d2f8b1d8ab00
kakas <e.kakas@student.fontys.nl>
2023-04-27 14:13:31 +0200

    removed codana file from user service and incorrect tests

--- surfSessionLogger/surfSessionAdmin/tests.py
+++ surfSessionLogger/surfSessionAdmin/tests.py
@@ -1,8 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
-
-
-
+from .models import SurfSession
+import json
 
 
 # Create your tests here.
@@ -10,27 +9,8 @@ from django.urls import reverse
 class SurfSessionApiTests(TestCase):
     
     def test_get_sessions(self):
-        response = self.client.get('/surfsession/')
-        self.assertEqual(response.status_code, 200)
-    
-    def test_post_sessions(self):
-        url ='/surfsession/'
-        response = self.client.post(url, {
-            "date_time": "2023-01-01T11:05",
-            "country": "NL",
-            "spot": "Scheveningen",
-            "rating": 3
-            }, format='json')
-
-        self.assertEqual(response.status_code, 201)
-    
-    def test_patch_session(self):
-        response = self.client.patch('/surfsession/1/', {
-            "date_time": "2023-01-01T11:05",
-            "country": "NL",
-            "spot": "Scheveningen",
-            "rating": 1
-            }, format='json')
+        headers = {'X-Consumer-Custom-ID': 1}
+        response = self.client.get('/surfsession/', headers=headers)
         self.assertEqual(response.status_code, 200)
 
 
