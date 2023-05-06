df92d4b54befb9aa38f622e9d0dbd06bd70ed8c2
kakas <e.kakas@student.fontys.nl>
2023-05-06 16:54:42 +0200

    cypress config files

---
+++ Ionic-Front-app/cypress/e2e/spec.cy.ts
@@ -0,0 +1,10 @@
+describe('ST spec', () => {
+  /* ==== Test Created with Cypress Studio ==== */
+  it('main', function() {
+    /* ==== Generated with Cypress Studio ==== */
+    cy.visit('localhost:8100');
+    /* ==== End Cypress Studio ==== */
+  });
+
+
+})
