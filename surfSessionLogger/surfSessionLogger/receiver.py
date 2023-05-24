fbeb6401b6c630891768a04966552a2a1d5719a8
kakas <e.kakas@student.fontys.nl>
2023-05-24 11:31:11 +0200

    rabbit mq kubernetes files added, changed rabbit connection urls and logger docker file

--- surfSessionLogger/surfSessionLogger/receiver.py
+++ surfSessionLogger/surfSessionLogger/receiver.py
@@ -2,7 +2,8 @@ import pika
 from surfSessionAdmin.models import SurfSession
 
 def start():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='surftracker-rabbitmq-1'))
+    #connection = pika.BlockingConnection(pika.ConnectionParameters(host='surftracker-rabbitmq-1'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='http://188.166.135.152:5672/'))
     channel = connection.channel()
 
     channel.queue_declare(queue='gdpr')
--- userService/userServiceAdmin/message_sender.py
+++ userService/userServiceAdmin/message_sender.py
@@ -2,7 +2,9 @@ import pika
 import json
 
 def send(user_id):
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='surftracker-rabbitmq-1'))
+    #connection = pika.BlockingConnection(pika.ConnectionParameters(host='surftracker-rabbitmq-1'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='http://188.166.135.152:5672/'))
+    
     channel = connection.channel()
     channel.queue_declare(queue='gdpr')
     json_str = json.dumps(user_id)
