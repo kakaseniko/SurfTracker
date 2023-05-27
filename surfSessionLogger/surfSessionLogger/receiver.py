fcb47186632adbaea1417e6966dd160728f3c661
kakas <e.kakas@student.fontys.nl>
2023-05-27 19:17:50 +0200

    session logger rabbitmq + dockerfile fix

--- surfSessionLogger/surfSessionLogger/receiver.py
+++ surfSessionLogger/surfSessionLogger/receiver.py
@@ -5,13 +5,16 @@ import os
 def start():
     host = os.environ.get('RABBIT_HOST')
     #connection = pika.BlockingConnection(pika.ConnectionParameters(host='surftracker-rabbitmq-1'))
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='188.166.135.152'))
+    #connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
     channel = connection.channel()
 
     channel.queue_declare(queue='gdpr')
 
-    def callback(ch, method, properties, body):
-        SurfSession.objects.filter(user_id=body.decode('utf-8')).delete()
+    def callback(ch, method, properties, body):   
+        id_string = body.decode('utf-8')
+        id = int(id_string.strip('"'))
+        SurfSession.objects.filter(user_id=id).delete()
 
         print(" [x] Received," )
 
--- userService/userServiceAdmin/message_sender.py
+++ userService/userServiceAdmin/message_sender.py
@@ -2,9 +2,10 @@ import pika
 import json
 import os
 def send(user_id):
-    host = os.environ.get('RABBIT_HOST')
+    #host = os.environ.get('RABBIT_HOST')
     #connection = pika.BlockingConnection(pika.ConnectionParameters(host='surftracker-rabbitmq-1'))
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='188.166.135.152'))
+    #connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
     
     channel = connection.channel()
     channel.queue_declare(queue='gdpr')
